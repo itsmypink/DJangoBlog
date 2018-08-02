e96451d2fbf12f0b74dd28eddc91a9d5712bfebe
unknown <itsmypink@sina.com>
2018-08-02 21:22:31 +0800

    first commit 1.0.0

---
+++ Blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your models here.
+class Category (models.Model):
+	name = models.CharField(max_length=100)
+	
+	
+class Tag(models.Model):
+	name=models.CharField(max_length=100)
+	
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	body=models.TextField()
+	created_time=models.DateTimeField()
+	modfied_time=models.DateTimeField()
+	excerpt=models.CharField(max_length=200,blank=True)
+	category=models.ForeignKey(Category)
+	tags=models.ManyToManyField(Tag,blank=True)
+	author=models.ForeignKey(User)
+	
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('Blog:detail', kwargs={'pk': self.pk})
+	class Meta:
+		ordering=["-created_time"]
---
+++ Blog/views.py
@@ -0,0 +1,36 @@
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+# Create your views here.
+import markdown
+from comments.forms import CommentForm
+
+from django.http import HttpResponse
+
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'Blog/index.html', context={'post_list': post_list})
+	
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    # 记得在顶部导入 CommentForm
+    form = CommentForm()
+    # 获取这篇 post 下的全部评论
+    comment_list = post.comment_set.all()
+
+    # 将文章、表单、以及文章下的评论列表作为模板变量传给 detail.html 模板，以便渲染相应数据。
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
+	
+	
+def archives(request, year, month):
+	post_list = Post.objects.filter(created_time__year=year,created_time__month=month)
+	return render(request, 'Blog/index.html', context={'post_list': post_list})
